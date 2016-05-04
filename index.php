3b2cf47eb0301e6cd58172f85c4f517139fa5b5e
weijiand <weijiand@usc.edu>
2016-05-04 14:40:38 -0700

    Added files via upload

--- index.php
+++ index.php
@@ -17,5 +17,21 @@ return $contents;
 else:
 echo "No symbol received.";
 endif;
+if ($_GET["lookup"]):
+$contentURL = 'http://dev.markitondemand.com/MODApis/Api/v2/Lookup/json?symbol=' .$_GET["lookup"];
+//echo $contentURL . "<br>111";
+$contents = file_get_contents($contentURL);
+//echo $contents . "<br>222";
+$contents = utf8_encode($contents);
+//echo $contents . "<br>pos1";
+$j_results = json_decode($contents);
+//var_dump($j_results);
+//echo "<br>pos2";
+//echo 'contents:';
+echo $_GET['callback']. "(" . $contents . ")";
+return $contents;
+else:
+echo "No symbol received.";
+endif;
 ?>
 
