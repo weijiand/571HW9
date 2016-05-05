d25beb1d30208dd25e8596e3babcc6f184722e5d
weijiand <weijiand@usc.edu>
2016-05-05 02:40:26 -0700

    Added files via upload

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ $context = stream_context_create(array(
 	)	
 )
 $contentURL = 'https://api.datamarket.azure.com/Bing/Search/v1/News?Query=%27' .$_GET["bingsymbol"] .'%27&$format=json';
-//echo $contentURL . "<br>111";
+echo $contentURL . "<br>111";
 $contents = file_get_contents($contentURL, 0, $context);
 //echo $contents . "<br>222";
 $contents = utf8_encode($contents);
